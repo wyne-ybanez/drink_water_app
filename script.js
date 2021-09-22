3ea42e0d0a5c26580d3d376aea18c2cca709079b
wyne-ybanez <wyne_01@icloud.com>
2021-09-22 19:49:39 +0100

    add update big cup function

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
+updateBigCup()
+
 smallcups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highLightCups(idx))
 })
 
 function highLightCups(idx) {
-
     // If the one we click is full and the next is not, decrease index by 1
     // Can toggle last highlighted cup
     if(smallcups[idx].classList.contains('full') && !smallcups[idx].nextElementSibling.classList.contains('full')) {
@@ -19,9 +20,33 @@ function highLightCups(idx) {
     smallcups.forEach((cup, idx2) => {
         if(idx2 <= idx) {
             cup.classList.add('full')
-        }
-        else {
+        } else {
             cup.classList.remove('full')
         }
     })
+
+    updateBigCup()
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallcups.length
+
+    if(fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+
+    // Hide remained text if full
+    if(fullCups === totalCups) {
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    } else {
+        remained.style.visibility = 'visible'
+        liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+    }
 }
